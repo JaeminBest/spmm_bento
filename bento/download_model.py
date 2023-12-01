b79927b818daaedbb751fb655e4086e06a466767
Jinho Chang <59189526+jinhojsk515@users.noreply.github.com>
2023-12-01 15:29:25 +0900

    Update download_model.py

--- bento/download_model.py
+++ bento/download_model.py
@@ -5,7 +5,7 @@ import torch
 import numpy as np
 
 
-model = get_model("./Pretrain/checkpoint_SPMM_20m.ckpt", device='cuda')
+model = get_model("./Pretrain/checkpoint_SPMM.ckpt", device='cuda')
 
 print(model(mode='p2s', p2s_prop=calculate_property('COc1cccc(NC(=O)CN(C)C(=O)COC(=O)c2cc(c3cccs3)nc3ccccc23)c1').numpy(), p2s_mask=np.zeros(53),
             stochastic=True, k=2, n_sample=5))
